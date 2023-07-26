db909cc780ca3a21a8761981d10cb713e63d2ac9
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-26 14:51:25 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Table from "./components/Wages/Wages";
 import Form from "./components/Form/Form";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <Form />
-      <Table />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Form />} />
+          <Route path="/epf" element={<Table />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
-import logo from "../../assets/cooptex.png"
+import React from "react";
+import logo from "../../assets/cooptex.png";
 import "./Form.scss";
+import { useNavigate } from "react-router-dom";
 
 const Form = () => {
-  const [name, setName] = useState("");
-  const [pfNumber, setPfNumber] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Name:", name);
-    console.log("PF Number:", pfNumber);
+    navigate("/epf");
   };
 
   return (
@@ -21,21 +20,11 @@ const Form = () => {
         <form className="form-container" onSubmit={handleSubmit}>
           <div className="form-name">
             <label htmlFor="name">Employee Name: </label> <br />
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
+            <input type="text" id="name" />
           </div>
           <div className="form-uan">
             <label htmlFor="uan">Enter the PF number: </label> <br />
-            <input
-              type="text"
-              id="uan"
-              value={pfNumber}
-              onChange={(e) => setPfNumber(e.target.value)}
-            />
+            <input type="text" id="uan" />
           </div>
           <div className="form-submit">
             <button type="submit">Submit</button>
