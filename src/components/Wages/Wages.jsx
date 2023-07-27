ea070583d9a7c5d3a2422772f057c286ebfa57e6
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-07-27 20:11:51 +0530

    removed pdf

--- src/components/Wages/Wages.jsx
+++ src/components/Wages/Wages.jsx
@@ -1,22 +1,13 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./Wages.scss";
-import { useReactToPrint } from "react-to-print";
 import { useParams } from "react-router-dom";
 
 const Wages = () => {
   const [epf, setEpf] = useState([]);
 
   let params = useParams();
-
-  const componentPDF = useRef();
-
-  const generatePDF = useReactToPrint({
-    content: () => componentPDF.current,
-    documentTitle: "Cooptex EPF",
-    onAfterPrint: () => alert("EPF Data saved!!!"),
-  });
 
   const getEpf = () => {
     axios.get(`http://localhost:3001/epf/${params.uan}`).then((res) => {
@@ -31,12 +22,7 @@ const Wages = () => {
   return (
     <div className="wages">
       <h1>Cooptex EPF</h1>
-      <button onClick={generatePDF}>Download PDF</button>
-      <div
-        className="table"
-        ref={componentPDF}
-        style={{ width: "100vw", padding: "20px" }}
-      >
+      <div className="table">
         <table>
           <thead>
             <tr>
@@ -49,7 +35,7 @@ const Wages = () => {
           <tbody>
             {epf.map((item, index) => (
               <tr key={item.s_no}>
-                <td>{index+1}</td>
+                <td>{index + 1}</td>
                 <td>{item.uan}</td>
                 <td>{item.name}</td>
                 <td>{item.wages}</td>
