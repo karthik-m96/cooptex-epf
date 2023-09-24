8a1af42fc6b8319e9407e1506f2627d4fb805e3b
Coding Habits Pro <learn2codeedaily@gmail.com>
2023-09-24 18:22:50 +0530

    DB migration issue resolved, MYSQL 2 upgraded

--- functions/api.js
+++ functions/api.js
@@ -1,20 +1,22 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const app = express();
 const serverless = require('serverless-http');
 const router = express.Router();
 
 const db = mysql.createConnection(process.env.DATABASE_URL);
+ db.connect();
+
 
 router.get("/epf/:uan", (req, res) => {
-  const sqlQuery = `select * from  2017to2023 where uan=${req.params.uan}`;
-  db.query(sqlQuery, (err, result) => {
+  const sqlQuery = `SELECT * FROM 2017to2023 where uan=${req.params.uan}`;
+  db.query(sqlQuery, (err, rows, fields) => {
     if (err) {
       console.log(err);
       res.status(500).send("Internal Server Error");
     } else {
-      res.send(result[0]);
+      res.send(rows);
     }
   });
 });
