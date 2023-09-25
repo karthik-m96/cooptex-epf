bbd9f6398fee1ca202c46dcd8d04e439d6f95ffa
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-09-25 11:15:21 +0530

    DB Query error rectified for form 3A

--- functions/api.js
+++ functions/api.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection(process.env.DATABASE_URL);
 
 
 router.get("/epf/:uan", (req, res) => {
-  const sqlQuery = `SELECT * FROM 2017to2023 where uan=${req.params.uan}`;
+  const sqlQuery = `select uan,name,month,wages,work_share,epf_diff_amount,pen_contr,already_remitted,difference_amount,year,month from 2017to2023 where uan=${req.params.uan}`;
   db.query(sqlQuery, (err, rows, fields) => {
     if (err) {
       console.log(err);
